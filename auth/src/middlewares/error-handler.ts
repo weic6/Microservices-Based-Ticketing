755eb5291eb72f2f681dc57f60b0dd9a6429100c
weic6 <wei.rarr@gmail.com>
2025-01-12 16:21:28 +0800

    convert errors to responses

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,10 +1,26 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went wrong", err);
-  res.status(400).send({ message: err.message });
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      if (error.type === "field") {
+        return { message: error.msg, field: error.path };
+      }
+    });
+    res.status(400).send({ errors: formattedErrors });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    res.status(500).send({ errors: [{ message: err.reason }] });
+  }
+
+  // other error
+  res.status(400).send({ errors: [{ message: "Something went wrong" }] });
 };
