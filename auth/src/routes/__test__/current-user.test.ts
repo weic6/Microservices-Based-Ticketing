2973203ec606a3f88e92b730401c2780ab11a07f
weic6 <wei.rarr@gmail.com>
2025-01-26 17:04:19 -0600

    add auth helper func; add non-authed req testing

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,15 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("responds with details about the current user", async () => {
-  const authResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
-
-  const cookie = authResponse.get("Set-Cookie");
+  const cookie = await global.signin();
 
   if (!cookie) {
     throw new Error("Cookie not set after signup");
@@ -24,4 +16,13 @@ it("responds with details about the current user", async () => {
 
   // console.log(response.body);
   expect(response.body.currentUser.email).toEqual("test@test.com");
+});
+
+it("responds with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
 });
