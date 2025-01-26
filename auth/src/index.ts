801192013fe3df161a65dc0db9aac91329a45172
weic6 <wei.rarr@gmail.com>
2025-01-26 08:02:25 -0600

    add session support

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import "express-async-errors";
 // import { json } from "body-parser";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -12,8 +13,15 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 // app.use(json());
 app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true, //require http connection
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
