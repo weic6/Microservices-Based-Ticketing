8d285a6d9291f322df91ada851c4ff889566fae2
weic6 <wei.rarr@gmail.com>
2025-01-27 06:22:07 -0600

    specify the correct host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,32 @@
-const Landing = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  axios.get("/api/users/currentuser").catch((err) => {
+    console.log(err.message);
+  });
+
   return <h1>Landing Page</h1>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    // We are on the server!
+    // Requests should be made to http://controller-nginx.ingress-nginx.svc.cluster.local
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // We are on the browser!
+    // Requests can be made with a base url of ""
+    const { data } = await axios.get("/api/users/currentuser");
+
+    return data;
+  }
+};
+
+export default LandingPage;
