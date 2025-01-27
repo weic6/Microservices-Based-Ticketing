1fa2661109b9978ced4915540852f6da205b718c
weic6 <wei.rarr@gmail.com>
2025-01-27 15:25:40 -0600

    validate title and price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,11 +1,23 @@
 import express, { Request, Response } from "express";
-import { requireAuth } from "@wchentickets/common";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@wchentickets/common";
 
 const router = express.Router();
 
-router.post("/api/tickets", requireAuth, (req: Request, res: Response) => {
-  res.status(200).send({});
-  return;
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.status(200).send({});
+    return;
+  }
+);
 
 export { router as createTicketRouter };
