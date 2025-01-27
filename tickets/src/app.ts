06fdf4cd0eba110ea7568d13a06ea581e4824ae6
weic6 <wei.rarr@gmail.com>
2025-01-27 13:43:36 -0600

    setup project

---
+++ tickets/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import "express-async-errors";
+// import { json } from "body-parser";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@wchentickets/common";
+
+const app = express();
+app.set("trust proxy", true);
+// app.use(json());
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test", //if true, it require https connection
+  })
+);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
