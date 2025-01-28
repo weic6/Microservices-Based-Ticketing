3f05cbc6c5da8b43f16e732b6a4e7e437b481a38
weic6 <wei.rarr@gmail.com>
2025-01-28 16:15:47 -0600

    use manual ack mode in NATS

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,14 +10,20 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  const subscription = stan.subscribe("ticket:created");
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
   subscription.on("message", (msg: Message) => {
     const data = msg.getData();
 
     if (typeof data === "string") {
-      console.log(
-        `Received event #${msg.getSequence()}, with data: ${JSON.parse(data)}`
-      );
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
