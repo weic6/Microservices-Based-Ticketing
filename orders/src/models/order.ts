b8dd84be997b3532501c867117179e1f88536b57
weic6 <wei.rarr@gmail.com>
2025-01-29 15:39:47 -0600

    use convenience document method

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { OrderStatus } from "@wchentickets/common";
 import { TicketDoc } from "./ticket";
+
+export { OrderStatus };
 interface OrderAttrs {
   userId: string;
   status: OrderStatus;
