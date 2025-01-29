877d1b46ad21caa608df30b40be263fbfbd8f00f
weic6 <wei.rarr@gmail.com>
2025-01-29 12:21:13 -0600

    add order service [u353]

---
+++ orders/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import "express-async-errors";
+// import { json } from "body-parser";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@wchentickets/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+app.set("trust proxy", true);
+// app.use(json());
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test", //if true, it require https connection
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
