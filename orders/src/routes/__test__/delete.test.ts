31bf58fa1fb786868f19afa5cdc63e952b5ffe57
weic6 <wei.rarr@gmail.com>
2025-01-29 16:51:31 -0600

    testing to cancel an order

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,33 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+import mongoose from "mongoose";
+
+it("marks an order as cancelled", async () => {
+  // Create a ticket with Ticket Model
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  // Make a request to create an order
+  const user = global.signin();
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  // Expectation to make sure the order is cancelled
+  const updatedOrder = await Order.findById(order.id);
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,31 @@
 import express, { Request, Response } from "express";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@wchentickets/common";
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
 router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
+  const { orderId } = req.params;
+  const order = await Order.findById(orderId);
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  // Publish an event saying that an order was cancelled
+
+  res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
