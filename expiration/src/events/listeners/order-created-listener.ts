c6407ebac5f8c2d37c14f5376ea1ae1d66ae8c03
weic6 <wei.rarr@gmail.com>
2025-01-30 16:52:13 -0600

    queue a job on event arrival

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,15 +1,17 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@wchentickets/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    // Find the ticket the the order is reserving
-    // Mark the ticket as being reserved by setting its orderId property
-    // Save the ticket
-    // ack the message
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
   }
 }
