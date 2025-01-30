de9a3ec047f3a67986e3b953c85668c525a31dcb
weic6 <wei.rarr@gmail.com>
2025-01-30 13:58:25 -0600

    add: reserving a ticket

--- tickets/src/events/listeners/order-created-listeners.ts
+++ tickets/src/events/listeners/order-created-listeners.ts
@@ -1,14 +1,27 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@wchentickets/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    console.log("Event data!", data);
+    // Find the ticket the the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
 
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // Save the ticket
+    await ticket.save();
+
+    // ack the message
     msg.ack();
   }
 }
