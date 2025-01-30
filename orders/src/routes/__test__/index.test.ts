29463ad552fe96513e4a4cf9c791de1317fda92e
weic6 <wei.rarr@gmail.com>
2025-01-30 13:43:21 -0600

    fix some tests

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
@@ -5,6 +6,7 @@ import { Order, OrderStatus } from "../../models/order";
 
 const buildTicket = async () => {
   const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
     title: "Concert",
     price: 20,
   });
