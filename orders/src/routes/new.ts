ad3c7490a977e83bc707f711dadc0954b8ac11e6
weic6 <wei.rarr@gmail.com>
2025-01-30 12:57:51 -0600

    abstracted query method; add version number in other services

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -62,9 +62,10 @@ router.post(
     // Publish an event saying that an order was created
     new OrderCreatedPublisher(natsWrapper.client).publish({
       id: order.id,
+      version: order.version,
       status: order.status,
       userId: order.userId,
-      expiresAt: order.expiresAt.toISOString(), // convert to string to avoid timezone issues
+      expiresAt: order.expiresAt.toISOString(),
       ticket: {
         id: ticket.id,
         price: ticket.price,
