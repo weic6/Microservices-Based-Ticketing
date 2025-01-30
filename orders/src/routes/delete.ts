a05ea7285a27d017f7aadebdf3527dda1f03fd31
weic6 <wei.rarr@gmail.com>
2025-01-29 19:17:46 -0600

    publish order cancellation

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,12 +5,14 @@ import {
   requireAuth,
 } from "@wchentickets/common";
 import { Order, OrderStatus } from "../models/order";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
 router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
   const { orderId } = req.params;
-  const order = await Order.findById(orderId);
+  const order = await Order.findById(orderId).populate("ticket"); // without populate, cannot access order.ticket!!
 
   if (!order) {
     throw new NotFoundError();
@@ -24,6 +26,12 @@ router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
   await order.save();
 
   // Publish an event saying that an order was cancelled
+  new OrderCancelledPublisher(natsWrapper.client).publish({
+    id: order.id,
+    ticket: {
+      id: order.ticket.id,
+    },
+  });
 
   res.status(204).send(order);
 });
