249f847090d815bc9a4d5861ac7b7b8b32d483a6
weic6 <wei.rarr@gmail.com>
2025-01-30 02:20:18 -0600

    testing optimistic concurrency control

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,3 +1,32 @@
 import { Ticket } from "../ticket";
 
-it("implements optimistic concurrency control", async (done) => {});
+it("implements optimistic concurrency control", async () => {
+  // create an instance of a ticket
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 5,
+    userId: "123",
+  });
+
+  // save the ticket to the database
+  await ticket.save();
+
+  // fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // make two separate changes to the tickets we fetched
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+
+  // save the first fetched ticket
+  await firstInstance!.save();
+  // save the second fetched ticket and expect an error
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return;
+  }
+
+  throw new Error("Should not reach this point");
+});
