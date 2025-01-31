ae5a67b0ecbd9a2c2246b117b56897c024fd8223
weic6 <wei.rarr@gmail.com>
2025-01-31 01:14:03 -0600

    paying for an order

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,18 @@
 import { useEffect, useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import useRequest from "../../hooks/use-request";
 
 const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: "/api/payments",
+    method: "post",
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payment) => console.log(payment),
+  });
+
   useEffect(() => {
     const findTimeLeft = () => {
       const msLeft = new Date(order.expiresAt) - new Date();
@@ -25,11 +35,12 @@ const OrderShow = ({ order, currentUser }) => {
     <div>
       Time left to pay: {timeLeft} seconds
       <StripeCheckout
-        token={(token) => console.log(token)}
+        token={({ id }) => doRequest({ token: id })}
         stripeKey="pk_test_51QnA3XGhs4ZcmEqisidjiDsCpPAZsHCm2SaxKeC1nthrHwV19yXPSPgq0rJZYPA17goGUwtBGLwEpIyOdz7Xco5r00n0RyX7Mg"
         amount={order.ticket.price * 100}
         email={currentUser.email}
       />
+      {errors}
     </div>
   );
 };
