7c9f67f781cf4d53e44ccf1ee35728626ceb204f
weic6 <wei.rarr@gmail.com>
2025-01-30 23:16:34 -0600

    mark an order as complete

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import {
 import { stripe } from "../stripe";
 import { Order } from "../models/order";
 import { Payment } from "../models/payment";
+import { PaymentCreatedPublisher } from "../events/publishers/payment-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -51,7 +53,13 @@ router.post(
     });
     await payment.save();
 
-    res.status(201).send({ success: true });
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    }); // await is not used here because we don't care if the event is successfully published and we don't want to wait for it
+
+    res.status(201).send({ id: payment.id });
   }
 );
 
