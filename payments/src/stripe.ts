1ca5a8d4907e4fed540eda728c0e30573c8639ae
weic6 <wei.rarr@gmail.com>
2025-02-01 22:14:39 -0600

    update stripe secret path

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -1,4 +1,11 @@
 import Stripe from "stripe";
+import dotenv from "dotenv";
+import path from "path";
+
+// Load the .env file only if not running in Kubernetes
+if (process.env.NODE_ENV !== "kubernetes") {
+  dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+}
 
 export const stripe = new Stripe(process.env.STRIPE_KEY!, {
   apiVersion: "2025-01-27.acacia",
